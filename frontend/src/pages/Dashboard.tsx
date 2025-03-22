13ac100e4ae427e1705cb4202f5605d8abbe81cb
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-03-23 01:45:53 +0530

    nothing

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,9 +2,9 @@ import SideNavigation from "@/components/SideNavBar";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="flex flex-col md:flex-row ml-4">
       <SideNavigation />
-      <div className="flex-1">
+      <div className="flex flex-col items-center justify-center flex-grow">
         <h1 className="text-2xl font-semibold">Dashboard</h1>
         <p>Welcome to the dashboard</p>
       </div>
